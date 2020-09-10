2c8cedcab91d740664af5bcec9d72e459b86acea
Endri Gjiri <egjiri@gmail.com>
2020-09-09 21:48:58 -0400

    Fix build errors from the store test file

--- src/store.test.ts
+++ src/store.test.ts
@@ -4,10 +4,8 @@ import JSONAPISerializer from './json-api/serializer';
 import { testCreateModel } from './serializer.test';
 
 test('test Store', async () => {
-  let store, result;
-
-  store = new Store(new JSONAPIAdapter({ host: 'https://www.zoocasa.com', namespace: 'services/api/v3', modelName: 'Listing' }), new JSONAPISerializer(testCreateModel));
-  result = await store.query({ filter: { status: 'available' }, page: { size: 2 }});
+  let store = new Store(new JSONAPIAdapter({ host: 'https://www.zoocasa.com', namespace: 'services/api/v3', modelName: 'Listing' }), new JSONAPISerializer(testCreateModel));
+  let result: any = await store.query({ filter: { status: 'available' }, page: { size: 2 }});
   expect(result.response.status).toEqual(200);
   expect(result.length).toEqual(2);
   expect(result.meta.pageSize).toEqual(2);
