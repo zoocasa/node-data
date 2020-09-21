57568f130ae9f1a1801b09530c787d644563353d
Mikaal <matthewnaik@gmail.com>
2020-09-21 16:04:38 -0400

    Add relationships to seralizer content

--- src/json-api/serializer.test.ts
+++ src/json-api/serializer.test.ts
@@ -6,6 +6,7 @@ test('test JSONAPISerializer', () => {
     data: [
       { id: 1, type: 'users', attributes: { 'first-name': 'John' }},
       { id: 2, type: 'users', attributes: { 'first-name': 'Jane' }},
+      { id: 3, type: 'users', attributes: { 'first-name': 'Joe' }, relationships: { friend: { data: { id: 1894, type: 'friends' }}}},
     ],
     meta: {
       'page-number': 1,
@@ -15,6 +16,6 @@ test('test JSONAPISerializer', () => {
     },
   };
   const response = new Serializer(testCreateModel).normalizePayload(payload);
-  expect(response).toEqual([{ id: 1, firstName: 'John' }, { id: 2, firstName: 'Jane' }]);
+  expect(response).toEqual([{ id: 1, firstName: 'John' }, { id: 2, firstName: 'Jane' }, { id: 3, firstName: 'Joe', friend: { id: 1894 }}]);
   expect(response.meta).toEqual({ pageNumber: 1, pageSize: 25, totalPages: 1, totalCount: 2 });
 });
--- src/json-api/serializer.ts
+++ src/json-api/serializer.ts
@@ -3,7 +3,9 @@ import Serializer from '../serializer';
 export default class JSONAPISerializer extends Serializer {
   public normalizePayload(payload: jsonApiPayload) {
     if (Array.isArray(payload.data)) {
-      const content = payload.data.map(item => this.buildModel({ id: item.id, ...item.attributes }));
+      const content = payload.data.map(item => {
+        return this.buildModel({ id: item.id, ...item.attributes, ...extractRelationships(item.relationships) });
+      });
       return this.proxyContent(content, payload.meta);
     } else {
       const { id, attributes } = payload.data as jsonApiResource;
@@ -12,10 +14,24 @@ export default class JSONAPISerializer extends Serializer {
   }
 }
 
+function extractRelationships(relationshipsData: jsonApiResource['relationships'] = {}) {
+  const relationships: Record<string, { id: string | number}> = {};
+  Object.keys(relationshipsData).forEach(relationship => {
+    relationships[relationship] = { id: relationshipsData[relationship].data.id };
+  });
+  return relationships;
+}
+
 type jsonApiResource = {
   id: number | string,
   type: string,
   attributes: Record<string, unknown>,
+  relationships?: Record<string, {
+    data: {
+      id: string | number,
+      type: string
+    }
+  }>
 }
 
 type jsonApiPayload = {
